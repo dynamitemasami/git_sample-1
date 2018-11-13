3ebc341c52e1c37780aca597f7bbc3ba11759340
masami <dynamitewasabi@gmail.com>
2018-11-13 20:47:43 +0900

    multiple sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 
 int sum(int,int);
+int multiple(int, int);
 
 int main()
 {
     int a=1; int b=9;
     std::cout << sum(a,b) << std::endl;
+    std::cout << multiple(a, b) << std::endl;
     return 0;
 }
 
 int sim(int a, int b)
 {
     return a+b;
+}
+int multiple(int a, int b)
+{
+    return a*b;
 }
